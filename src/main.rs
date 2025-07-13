7b259191ddf3d6ab2bfd8da1423a21a1596f2c06
LordAfterEight <elias@stettmayer.de>
2025-07-13 17:28:16 +0200

    Brown triangle thingy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+mod state;
+mod app;
+
+use state::State;
+use app::App;
+
+use winit::event_loop ::EventLoop;
+
 fn main() {
     println!("Hello, world!");
+    run();
+}
+
+pub fn run() -> anyhow::Result<()> {
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        env_logger::init();
+    }
+    #[cfg(target_arch = "wasm32")]
+    {
+        console_log::init_with_level(log::Level::Info).unwrap_throw();
+    }
+
+    let event_loop = EventLoop::with_user_event().build()?;
+    let mut app = App::new(
+        #[cfg(target_arch = "wasm32")]
+        &event_loop,
+    );
+    event_loop.run_app(&mut app)?;
+
+    Ok(())
 }
